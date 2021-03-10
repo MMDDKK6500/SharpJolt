8cf1a0ae52126f4f1f68738787f351d60ef1ef74
WoobieGames <woobiegames@gmail.com>
2021-03-10 10:50:13 -0300

    Added AuthUser

--- GameJoltAPIExample/Program.cs
+++ GameJoltAPIExample/Program.cs
@@ -11,7 +11,7 @@ namespace GameJoltAPIExample
         {
             GJCore c = new GJCore("86282091", "9021he90h9210");
             GJUser u = new GJUser(c, "MMDDKK", "0mad6");
-            GJTrophies t = new GJTrophies(c, u,  "245622");
+            GJTrophies t = new GJTrophies(c, u);
             Console.WriteLine(c.game_id);
             Console.WriteLine(t);
             Console.ReadKey();
