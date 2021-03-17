4bc9ed56cba266b4663c19c11be9ddb0c88000f9
Israel <yorichii.d@gmail.com>
2021-03-17 09:06:54 -0300

    Add lots of improvements

---
+++ src/GameJoltAPI/GJCore.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GamejoltAPI.Core
+{
+
+    public class GJCore
+    {
+        public const string APIUrl = "https://api.gamejolt.com/api/game/v1_2/";
+
+        private string gameId;
+        private string privateKey;
+
+        public string GameID
+        {
+            get => gameId;
+        }
+
+        public string PrivateKey
+        {
+            get => privateKey;
+        }
+
+        public GJCore(string gameId, string privateKey)
+        {
+            this.gameId = gameId;
+            this.privateKey = privateKey;
+        }
+
+        public async Task<string> GetTime()
+        {
+            var cmd = "time/?game_id=" + gameId;
+            var str = APIUrl + cmd + privateKey;
+            var response = await Tools.Get(APIUrl + cmd + "&signature=" + str.ToMD5Hash());
+            return response;
+        }
+
+    }
+
+    public static class Tools
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<string> Get(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(GJCore.APIUrl + url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                return $"An internal error has occured: {ex.Message}";
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string ToMD5Hash(this string input)
+        {
+            MD5 m = MD5.Create();
+            byte[] data = m.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sbuild = new StringBuilder();
+
+            for (int i = 0; i < data.Length; ++i)
+                sbuild.Append(data[i].ToString("x2"));
+
+            return sbuild.ToString();
+        }
+    }
+}
---
+++ src/GameJoltAPI/Sessions/GJSession.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using GamejoltAPI.Core;
+using GamejoltAPI.Users;
+using System.Threading.Tasks;
+
+namespace GamejoltAPI.Sessions
+{
+    public class GJSession
+    {
+        private string gameId;
+        private string username;
+        private string userToken;
+        private string privateKey;
+
+        public GJSession(GJCore core, GJUser user)
+        {
+            gameId = user.GameID;
+            username = user.Username;
+            userToken = user.Token;
+            privateKey = core.PrivateKey;
+        }
+
+        public async Task<string> Open()
+        {
+            var cmd = "sessions/open/?game_id=" + gameId + "&username=" + username + "&user_token=" + userToken;
+            var str = GJCore.APIUrl + cmd + privateKey;
+            var response = await Tools.Get(GJCore.APIUrl + cmd + "&signature=" + str.ToMD5Hash());
+            return response;
+        }
+
+        public async Task<string> Ping(string status)
+        {
+            var cmd = "sessions/ping/?game_id=" + gameId + "&username=" + username + "&user_token=" + userToken + "&status" + status;
+            var str = GJCore.APIUrl + cmd + privateKey;
+            var response = await Tools.Get(GJCore.APIUrl + cmd + "&signature=" + str.ToMD5Hash());
+            return response;
+        }
+
+        public async Task<string> Check()
+        {
+            var cmd = "sessions/check/?game_id=" + gameId + "&username=" + username + "&user_token=" + userToken;
+            var str = GJCore.APIUrl + cmd + privateKey;
+            var response = await Tools.Get(GJCore.APIUrl + cmd + "&signature=" + str.ToMD5Hash());
+            return response;
+        }
+
+        public async Task<string> Close()
+        {
+            var cmd = "sessions/close/?game_id=" + gameId + "&username=" + username + "&user_token=" + userToken;
+            var str = GJCore.APIUrl + cmd + privateKey;
+            var response = await Tools.Get(GJCore.APIUrl + cmd + "&signature=" + str.ToMD5Hash());
+            return response;
+        }
+
+    }
+}
